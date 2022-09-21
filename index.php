868e7f015eef7f705e0da4f0f83e0b776b94219d
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-09-21 14:34:48 -0500

    test

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost:3306";
+$username = "felixfin_user";
+$password = "*)q}({hr5#SQ";
+$dbname = "felixfin_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT Club, Standings FROM Teams";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+?>
+    <tr>
+        <td><?=$row["Club"]?></td>
+        <td><?=$row["Standings"]?></td>
+    </tr>
+
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>

931e0db9e2e965dbefdb11d41cec17017ac2ce12
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-09-26 17:14:52 -0500

    change sectioning of server connection back

--- index.php
+++ index.php
@@ -9,10 +9,20 @@
     </tr>
   </thead>
   <tbody>
-    
-<?php include 'server_connection.php';?>
 
     <?php
+    $servername = "localhost";
+    $username = "felixfin_user2";
+    $password = "O-,GXdw4e3QG";
+    $dbname = "felixfin_homework3";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    
 $sql = "SELECT Club, Standings FROM Teams";
 $result = $conn->query($sql);
 

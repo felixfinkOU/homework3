e2bc097a34ccfb4b7c111fe3c5c949a3369a6ce1
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-17 14:18:45 -0500

    Added Club Cards

---
+++ club-cards.php
@@ -0,0 +1,56 @@
+<?php include 'header.php';?>
+
+    <h1>Players</h1>
+<div class="card-group">
+    <?php
+$servername = "localhost";
+$username = "felixfin_user2";
+$password = "O-,GXdw4e3QG";
+$dbname = "felixfin_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT CLub, Standings from Teams";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["Club"]?></h5>
+      <p class="card-text"><ul>
+<?php
+    $section_sql = "select t.Standings, p.LastName, m.LastName
+                    from SoccerPlayer p join Teams t on t.Club = p.Club
+                                        join SoccerManagers m on t.Club = m.Club 
+                    where t.Club=" . $row["CLub"];
+    $section_result = $conn->query($section_sql);
+    
+    while($section_row = $section_result->fetch_assoc()) {
+      echo "<li>" . $section_row["Standings"] . "</li>";
+      echo "<li>" . $section_row["Player"] . "</li>";
+      echo "<li>" . $section_row["Manager"] . "</li>";
+    }
+?>
+      </ul></p>
+  </div>
+    </div>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </card-group>
+
+<a class="btn btn-primary" type="button" href="index.php">Go Back</a>
+
+<?php include 'footer.php';?>

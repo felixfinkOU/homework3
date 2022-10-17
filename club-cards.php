5651672e6043e709b0003eddf669089bdc7b0352
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-17 14:30:44 -0500

    changed syntax of sql

--- club-cards.php
+++ club-cards.php
@@ -27,10 +27,7 @@ if ($result->num_rows > 0) {
       <h5 class="card-title"><?=$row["Club"]?></h5>
       <p class="card-text"><ul>
 <?php
-    $section_sql = "select t.Standings, p.LastName, m.LastName
-                    from SoccerPlayer p join Teams t on t.Club = p.Club
-                                        join SoccerManagers m on t.Club = m.Club 
-                    where t.Club=" . $row["Club"];
+    $section_sql = "select t.Standings, p.LastName, m.LastName from SoccerPlayer p join Teams t on t.Club = p.Club join SoccerManagers m on t.Club = m.Club where t.Club=" . $row["Club"];
     $section_result = $conn->query($section_sql);
     
     while($section_row = $section_result->fetch_assoc()) {

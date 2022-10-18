e6dfd0b58653c315cfd17f4819feebcc5f6e71dc
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-18 11:25:15 -0500

    added css style

--- club-cards.php
+++ club-cards.php
@@ -1,7 +1,7 @@
 <?php include 'header.php';?>
 
     <h1>Clubs</h1>
-<div class="card-group" content="width=1000px, overflow=scroll">
+<div class="card-group">
     <?php
 $servername = "localhost";
 $username = "felixfin_user2";
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-   <div class="card">
+   <div class="card" style="width: 500px">
     <div class="card-body">
       <h5 class="card-title"><?=$row["Club"]?></h5>
       <p class="card-text"><ul>

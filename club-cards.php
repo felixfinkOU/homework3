4ab8c6a0c834866397a2e86eba0732a1db721408
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-18 11:37:32 -0500

    added more rows

--- club-cards.php
+++ club-cards.php
@@ -33,8 +33,8 @@ if ($result->num_rows > 0) {
     
     while($section_row = $section_result->fetch_assoc()) {
       echo "<li>" . $section_row["Standings"] . "</li>";
-      #echo "<li>" . $section_row["Player"] . "</li>";
-      #echo "<li>" . $section_row["Manager"] . "</li>";
+      echo "<li>" . $section_row["Player"] . "</li>";
+      echo "<li>" . $section_row["Manager"] . "</li>";
     }
 ?>
       </ul></p>

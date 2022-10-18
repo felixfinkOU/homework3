963deaef62aac8a1b59114e9e8e190d2ebe60cd2
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-18 11:33:26 -0500

    added scroll

--- club-cards.php
+++ club-cards.php
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-   <div class="card" style="min-width:10%;">
+   <div class="card" style="min-width:10%;overflow:scroll;">
     <div class="card-body">
       <h5 class="card-title"><?=$row["Club"]?></h5>
       <p class="card-text"><ul>

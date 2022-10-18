371df478c9aba21fe6e28f99a1ef8fd68cbe76cc
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-10-18 11:16:45 -0500

    changed width total page

--- club-cards.php
+++ club-cards.php
@@ -1,5 +1,12 @@
-<?php include 'header.php';?>
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1">
+    <title>Homework 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
     <h1>Clubs</h1>
 <div class="card-group">
     <?php
